55ba1d7b32df9f4dec12e8e966405c19d1f1d440
Larwinj <119966720+Larwincyber@users.noreply.github.com>
2023-11-07 16:12:52 +0530

    Add files via upload

---
+++ Day 2/src/Components/Caldr.js
@@ -0,0 +1,60 @@
+const Caldr = () =>{
+    return(
+        <div align = "center">
+            <table border="dotted" background="https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg">
+                <tr>
+                <th>SUNDAY</th>
+                <th>MONDAY</th>
+                <th>TUESDAY</th>
+                <th>WEDNESDAY</th>
+                <th>THURSDAY</th>
+                <th>FRIDAY</th>
+                <th>SATURDAY</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td>9</td>
+                    <td>10</td>
+                    <td>11</td>
+                    <td>12</td>
+                    <td>13</td>
+                    <td>14</td>
+                </tr>
+                <tr>
+                    <td>15</td>
+                    <td>16</td>
+                    <td>17</td>
+                    <td>18</td>
+                    <td>19</td>
+                    <td>20</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>22</td>
+                    <td>23</td>
+                    <td>24</td>
+                    <td>25</td>
+                    <td>26</td>
+                    <td>27</td>
+                    <td>28</td>
+                </tr>
+                <tr>
+                    <td>29</td>
+                    <td>30</td>
+                </tr>
+            </table>
+            {/* <img  src="https://huggingface.co/tasks/assets/image-classification/image-classification-input.jpeg" align = "right" id="cat"></img> */}
+        </div>
+
+    )
+}
+export default Caldr;
---
+++ Day 2/src/Components/Student.js
@@ -0,0 +1,10 @@
+const Student = ()=>{
+    return(
+        <div>
+            <h2 align="center">This is Mf</h2>
+            <p align="center">Don't Mess</p>
+            {/* <p align="center">November 2023</p> */}
+        </div>
+    )
+}
+export default Student;
